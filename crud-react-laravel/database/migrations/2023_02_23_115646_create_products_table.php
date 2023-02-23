dafd041952efb007367959dce1e33cbabefceae2
Mr-codehunter <codeonhunt@gmail.com>
2023-02-23 17:31:04 +0530

    set schema's

--- crud-react-laravel/database/migrations/2023_02_23_115646_create_products_table.php
+++ crud-react-laravel/database/migrations/2023_02_23_115646_create_products_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->text('image');
             $table->timestamps();
         });
     }
